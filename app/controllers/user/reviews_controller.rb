06b919d0d735358e64493e9083d727a2d098ae1c
TasakiFumiko <121810685+tasakifumiko@users.noreply.github.com>
2023-03-25 10:29:57 +0000

    非同期通信まで

--- app/controllers/user/reviews_controller.rb
+++ app/controllers/user/reviews_controller.rb
@@ -1,13 +1,21 @@
 class User::ReviewsController < ApplicationController
 
   def create
-    post = Post.find(params[:post_id])
+    @post = Post.find(params[:post_id])
+    @review= Review.new
+    @reviews = @post.reviews.where(visible: true)
     comment = current_user.reviews.new(review_params)
-    comment.post_id = post.id
+    comment.post_id = @post.id
     comment.save
-    redirect_to post_path(post)
   end
-  
+
+  def destroy
+    @post = Post.find(params[:post_id])
+    @review= Review.new
+    @reviews = @post.reviews.where(visible: true)
+    Review.find_by(id: params[:id], post_id: params[:post_id]).destroy
+  end
+
   def index
     @comments = Comment.all
   end
